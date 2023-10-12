a70738b1a2624f7bcec178b0044bd5575060b1b3
Chen12 <Chen12@916-IAM-134-01>
2023-10-12 17:42:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+using System.IO;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        SqlConnectionStringBuilder mySqlConBldr = new SqlConnectionStringBuilder();
+        mySqlConBldr["server"] = @"(localdb)\MSSQLLocalDB";
+        mySqlConBldr["Trusted_Connection"] = true;
+        mySqlConBldr["Integrated Security"] = "SSPI";
+        mySqlConBldr["Initial Catalog"] = "PROG260FA23";
+        string sqlConStr = mySqlConBldr.ToString();
+        Console.WriteLine(sqlConStr);
+
+        using(SqlConnection conn = new SqlConnection(sqlConStr))
+        {
+            conn.Open();
+
+        }
+    }
+}

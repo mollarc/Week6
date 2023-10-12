3858ac118e6dae54db4c6511a16e9fcc7e2690e8
Chen12 <Chen12@916-IAM-134-01>
2023-10-12 18:05:20 -0500

    Sql Connected and data being read

--- Program.cs
+++ Program.cs
@@ -12,11 +12,43 @@ internal class Program
         mySqlConBldr["Initial Catalog"] = "PROG260FA23";
         string sqlConStr = mySqlConBldr.ToString();
         Console.WriteLine(sqlConStr);
-
-        using(SqlConnection conn = new SqlConnection(sqlConStr))
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Files");
+        foreach(var file in files)
+        {
+            using(StreamReader sr = new StreamReader(file))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split('|');
+                    using(SqlConnection conn = new SqlConnection(sqlConStr))
+                    {
+                        conn.Open();
+                        string inlineSQL = $@"INSERT INTO [dbo].[Produce] ([Name],[Location],[Price],[UoM],[Sell_by_Date]) VALUES ('{fields[0]}','{fields[1]}','{fields[2]}','{fields[3]}','{fields[4]}')";
+                        using (var command = new SqlCommand(inlineSQL, conn))
+                        {
+                            var query = command.ExecuteNonQuery();
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+        }
+        using (SqlConnection conn = new SqlConnection(sqlConStr))
         {
             conn.Open();
-
+            string inlineSQL = @"Select * from Produce";
+            using (var command = new SqlCommand(inlineSQL, conn))
+            {
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var value = $"{reader.GetValue(0)},{reader.GetValue(1)},{reader.GetValue(2)},{reader.GetValue(3)},{reader.GetValue(4)}";
+                    Console.WriteLine(value);
+                }
+                reader.Close();
+            }
+            conn.Close();
         }
     }
 }
